9afe89268dac34c469821ff502669968ccb9a090
rahulsingh2310 <rahulkumar.s17@iiits.in>
2020-07-19 17:41:48 +0530

    initial commit1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import MapContainer from './map.js';
+
+function App() {
+  return (
+    <div>
+      <MapContainer />
+    </div>
+  );
+}
+
+export default App;
